9696246838131b60dbf27fd3dcce53364ade4fbe
Sandeep <skumar@gainsight.com>
2015-06-02 00:58:28 +0530

    Skipping logic and Execution context logic implemented.

--- src/main/java/com/dc/batch/impl/CustomItemProcessor.java
+++ src/main/java/com/dc/batch/impl/CustomItemProcessor.java
@@ -9,13 +9,14 @@ import org.springframework.batch.item.ItemProcessor;
 public class CustomItemProcessor implements ItemProcessor<UsageData, UsageData> {
 
 	@Override
-	public UsageData process(UsageData item)  {
-		try{
-			System.out.println("Processing batch " + item.getAccountName() +" "+item.getInstanceId().substring(1));
-		}catch (Exception e){
-			System.out.println("Exception "+e.getStackTrace());
-			return new UsageData();
-		}
+	public UsageData process(UsageData item) throws Exception {
+		//try{
+			System.out.println("UsageData : "+item.getAccountName()+" "+item.getInstanceId().substring(1)+" "+item.getInstanceName());
+			//System.out.println("Processing batch " + item.getAccountName() +" "+item.getInstanceId().substring(1));
+		/*}catch (Exception e){
+			System.out.println("Exception ");
+			throw new Exception("Exeption occ while processing");
+		}*/
 
 		return item;
 	}
--- src/main/java/com/dc/batch/impl/CustomItemWriter.java
+++ src/main/java/com/dc/batch/impl/CustomItemWriter.java
@@ -3,20 +3,46 @@ package com.dc.batch.impl;
 import com.dc.batch.Entry;
 import com.dc.batch.model.UsageData;
 import org.springframework.batch.core.launch.support.SimpleJobOperator;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemStream;
+import org.springframework.batch.item.ItemStreamException;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 
 import java.util.List;
 
 /**
  * Created by Sandeep on 23-05-2015.
  */
-public class CustomItemWriter implements ItemWriter<UsageData> {
+public class CustomItemWriter implements ItemWriter<UsageData>,ItemStream {
 
+    public static int count;
+    public static String batchCount;
+    FlatFileItemWriter<UsageData> delegate;
     @Override
     public void write(List<? extends UsageData> list) throws Exception {
-        System.out.println("Writing in a batch of  size " + list.size());
+        System.out.println("\n Writing in a batch of  size " + list.size()+" Count "+count++ +"\n");
+        batchCount = "Batch:"+count;
         for(String jobs : Entry.simpleJobOperator.getJobNames()){
             System.out.println(jobs);
         }
+    }
+
+    @Override
+    public void open(ExecutionContext executionContext) throws ItemStreamException {
+        System.out.println("Exceution context "+executionContext);
+       // delegate.open(executionContext);
+    }
+
+    @Override
+    public void update(ExecutionContext executionContext) throws ItemStreamException {
+        System.out.println("Exceution Update "+executionContext);
+       // delegate.update(executionContext);
+    }
+
+    @Override
+    public void close() throws ItemStreamException {
+        System.out.println("Exceution Close ");
+       // delegate.close();
     }
 }
